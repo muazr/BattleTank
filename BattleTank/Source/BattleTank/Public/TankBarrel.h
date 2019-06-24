c8095a1510491f1bd7efbda53ed69a9d63fc5c91
Muaz R <39170715+muazr@users.noreply.github.com>
2019-06-24 17:44:49 +0800

    Bug fixes
    - Disable collision for Tank Turret and Barrel as a workaround for ConvexElem invalid error messages
    - Delete and re-add Tank on BattleGround level to fix bug where the AI Tank would aim behind rather than at the player

--- BattleTank/Source/BattleTank/Public/TankBarrel.h
+++ BattleTank/Source/BattleTank/Public/TankBarrel.h
@@ -9,7 +9,7 @@
 /**
  * 
  */
-UCLASS( meta=(BlueprintSpawnableComponent), hidecategories=("Collision") )
+UCLASS( meta=(BlueprintSpawnableComponent) )
 class BATTLETANK_API UTankBarrel : public UStaticMeshComponent
 {
 	GENERATED_BODY()
