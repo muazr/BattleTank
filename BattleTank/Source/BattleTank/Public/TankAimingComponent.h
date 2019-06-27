f924b0f3ec5a8c95a52c7160d098c1132dd7e11b
Muaz R <39170715+muazr@users.noreply.github.com>
2019-06-27 13:46:55 +0800

    Set TankAimingComponent reference in TankPlayerController_BP for PlayerUI_BP temporarily

--- BattleTank/Source/BattleTank/Public/TankAimingComponent.h
+++ BattleTank/Source/BattleTank/Public/TankAimingComponent.h
@@ -33,7 +33,7 @@ public:
 
 protected:
 	UPROPERTY(BlueprintReadOnly, Category="State")
-	EFiringStatus FiringStatus = EFiringStatus::Reloading;
+	EFiringStatus FiringStatus = EFiringStatus::Aiming;
 
 private:
 	void MoveBarrelTowards(const FVector &AimDirection);
