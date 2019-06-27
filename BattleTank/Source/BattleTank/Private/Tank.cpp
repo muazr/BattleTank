muazr/BattleTank

commit 23be4dfad99356b0ef0a956b662e00c280545fcb

Refactor TankAimingComponent from default-spawned in C++ to spawnable in Blueprint
- Aiming doesn't work yet because pointer to TankAimingComponent in Tank is not set

BattleTank/Source/BattleTank/Private/Tank.cpp
@@ -13,8 +13,6 @@ ATank::ATank()
 {
  	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = false;
-
-	TankAimingComponent = CreateDefaultSubobject<UTankAimingComponent>(FName("AimingComponent"));
 }
 
 // Called when the game starts or when spawned
@@ -24,26 +22,10 @@ void ATank::BeginPlay()
 	
 }
 
-// Called to bind functionality to input
-void ATank::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
-{
-	Super::SetupPlayerInputComponent(PlayerInputComponent);
-
-}
-
-void ATank::SetBarrelReference(UTankBarrel * BarrelToSet)
-{
-	TankAimingComponent->SetBarrelReference(BarrelToSet);
-	Barrel = BarrelToSet;
-}
-
-void ATank::SetTurretReference(UTankTurret * TurretToSet)
-{
-	TankAimingComponent->SetTurretReference(TurretToSet);
-}
-
 void ATank::AimAt(const FVector &HitLocation)
 {
+	if (!TankAimingComponent) return;
+
 	TankAimingComponent->AimAt(HitLocation, LaunchSpeed);
 }
 

BattleTank/Source/BattleTank/Private/TankAimingComponent.cpp
@@ -18,14 +18,10 @@ UTankAimingComponent::UTankAimingComponent()
 	// ...
 }
 
-void UTankAimingComponent::SetBarrelReference(UTankBarrel * BarrelToSet)
-{
-	Barrel = BarrelToSet;
-}
-
-void UTankAimingComponent::SetTurretReference(UTankTurret * TurretToSet)
+void UTankAimingComponent::Initialize(UTankTurret *TurretToSet, UTankBarrel *BarrelToSet)
 {
 	Turret = TurretToSet;
+	Barrel = BarrelToSet;
 }
 
 void UTankAimingComponent::AimAt(const FVector &HitLocation, float LaunchSpeed)
@@ -56,6 +52,8 @@ void UTankAimingComponent::AimAt(const FVector &HitLocation, float LaunchSpeed)
 
 void UTankAimingComponent::MoveBarrelTowards(const FVector &AimDirection)
 {
+	if (!Barrel || !Turret) return;
+
 	FRotator BarrelRotator = Barrel->GetForwardVector().Rotation();
 	FRotator AimAsRotator = AimDirection.Rotation();
 	FRotator DeltaRotator = AimAsRotator - BarrelRotator;

BattleTank/Source/BattleTank/Public/Tank.h
@@ -24,25 +24,19 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-	UPROPERTY(BlueprintReadOnly)
-	UTankAimingComponent *TankAimingComponent = nullptr;
-
 public:	
-	// Called to bind functionality to input
-	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
-
-	UFUNCTION(BlueprintCallable, Category="Setup")
-	void SetBarrelReference(UTankBarrel *BarrelToSet);
-
-	UFUNCTION(BlueprintCallable, Category="Setup")
-	void SetTurretReference(UTankTurret *TurretToSet);
-
 	void AimAt(const FVector &HitLocation);
 
 	UFUNCTION(BlueprintCallable, Category="Firing")
 	void Fire();
 
+	UPROPERTY(BlueprintReadOnly)
+	UTankAimingComponent *TankAimingComponent = nullptr;
+
 private:
+	// Local barrel reference for spawning projectile
+	UTankBarrel *Barrel = nullptr; // TODO Remove
+
 	UPROPERTY(EditDefaultsOnly, Category="Firing")
 	float LaunchSpeed = 5000.0f;
 
@@ -54,6 +48,4 @@ private:
 
 	UPROPERTY(EditDefaultsOnly, Category="Firing")
 	double LastFireTime = 0.0;
-
-	UTankBarrel *Barrel = nullptr;
 };

BattleTank/Source/BattleTank/Public/TankAimingComponent.h
@@ -26,9 +26,8 @@ public:
 	// Sets default values for this component's properties
 	UTankAimingComponent();
 
-	void SetBarrelReference(UTankBarrel *BarrelToSet);
-
-	void SetTurretReference(UTankTurret *TurretToSet);
+	UFUNCTION(BlueprintCallable, Category="Setup")
+	void Initialize(UTankTurret *TurretToSet, UTankBarrel *BarrelToSet);
 
 	void AimAt(const FVector &HitLocation, float LaunchSpeed);
 

commit 2bad5d725c16e92dcbfd378cbea097b5cba8d7b7

Fix aiming and firing bug introduced in commit 23be4df before we finally refactor the architecture

BattleTank/Source/BattleTank/Private/Tank.cpp
@@ -5,7 +5,6 @@
 #include "TankAimingComponent.h"
 #include "TankBarrel.h"
 #include "Projectile.h"
-#include "TankMovementComponent.h"
 #include "Engine/World.h"
 
 // Sets default values
@@ -20,6 +19,9 @@ void ATank::BeginPlay()
 {
 	Super::BeginPlay();
 	
+	// TODO To refactor out to TankAimingComponent
+	TankAimingComponent = FindComponentByClass<UTankAimingComponent>();
+	Barrel = FindComponentByClass<UTankBarrel>();
 }
 
 void ATank::AimAt(const FVector &HitLocation)
@@ -31,9 +33,9 @@ void ATank::AimAt(const FVector &HitLocation)
 
 void ATank::Fire()
 {
-	bool isReloaded = (FPlatformTime::Seconds() - LastFireTime) > ReloadTimeInSeconds;
-
 	if (!ensure(Barrel)) return;
+
+	bool isReloaded = (FPlatformTime::Seconds() - LastFireTime) > ReloadTimeInSeconds;
 
 	if (isReloaded)
 	{

BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
@@ -11,14 +11,9 @@ void ATankPlayerController::BeginPlay()
 	Super::BeginPlay();
 
 	auto AimingComponent = GetControlledTank()->FindComponentByClass<UTankAimingComponent>();
-	if (ensure(AimingComponent))
-	{
-		FoundAimingComponent(AimingComponent);
-	}
-	else
-	{
-		UE_LOG(LogTemp, Warning, TEXT("Player Controller can't find aiming component at BeginPlay"));
-	}
+	if (!ensure(AimingComponent)) return;
+
+	FoundAimingComponent(AimingComponent);
 }
 
 void ATankPlayerController::Tick(float DeltaTime)